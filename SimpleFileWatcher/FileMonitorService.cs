d597a66956ddbc355151d9a65899c38a1f340fcf
Roger J <rogerjes@gmail.com>
2025-03-22 21:15:02 -0400

    Add project files.

---
+++ SimpleFileWatcher/FileMonitorService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SimpleFileWatcher
+{
+    internal class FileMonitorService: BackgroundService
+    {
+        private readonly string _directoryToWatch;
+        private readonly HashSet<string> _processedFiles = new HashSet<string>();
+
+        public FileMonitorService(string directoryToWatch)
+        {
+            _directoryToWatch = directoryToWatch;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            Console.WriteLine($"Staring to monitor directory for file additions: {_directoryToWatch}");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var files = Directory.GetFiles(_directoryToWatch);
+
+                foreach (var file in files)
+                {
+                    if (!_processedFiles.Contains(file))
+                    {
+                        Console.WriteLine($"New file detected: {file}");
+                        _processedFiles.Add(file);
+
+                        // add logic here to process the new file as needed
+                    }
+                }
+            }
+
+            // wait a time interval (5 seconds in this example) before checking for new files again
+            await Task.Delay(5000, stoppingToken);
+
+        }
+    }
+}
---
+++ SimpleFileWatcher/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SimpleFileWatcher
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // specify the directory to monitor (could be read from configuration for more flexibility)
+            string directoryToWatch = @"C:\temp\";
+
+            // create a new host to run the background service FileMonitorService
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileMonitorService>(provider => new FileMonitorService(directoryToWatch));
+                }).
+                Build();
+
+            await host.RunAsync();
+        }
+    }
+}
